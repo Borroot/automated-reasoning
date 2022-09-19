5046dbd1d46ae0b4749121065ad74b0a5cb8458a
borroot <inbox@borroot.com>
2022-09-19 10:39:00 +0200

    Beautify pallets

--- part1/pallets.py
+++ part1/pallets.py
@@ -1,59 +1,75 @@
 from z3 import *
 
-goods = {};
-goods['nuzzle'] = {'weight': 700, 'pallet': 4}
-goods['prittle'] = {'weight': 400}
-goods['skipple'] = {'weight': 1000, 'pallet': 8}
-goods['crottle'] = {'weight': 2500, 'pallet': 10}
-goods['dupple'] = {'weight': 200, 'pallet': 20}
+NUM_TRUCKS = 8
+TRUCK_CAPACITY = 8000
+TRUCK_MAX_PALLETS = 8
+
+# Create all the goods with their weight and number of pallets.
+goods = {
+    'nuzzle':  {'weight': 700,  'pallet': 4},
+    'skipple': {'weight': 1000, 'pallet': 8},
+    'crottle': {'weight': 2500, 'pallet': 10},
+    'dupple':  {'weight': 200,  'pallet': 20},
+    'prittle': {'weight': 400},
+};
+
+# Create all the z3 variables, an integer for every (good, truck) pair
+# representing the amount of that good in the truck.
 distribution = {}
 for good in goods.keys():
-	distribution[good] = [Int('{} {}'.format(good, truck)) for truck in range(8)]
-print(distribution)
+    distribution[good] = [Int('{} {}'.format(good, truck)) for truck in range(NUM_TRUCKS)]
 
+# Create the solver object, we optimize for the number of prittle pallets.
 solver = Optimize();
 
-for truck in range(8):
-	truckWeight = 0
-	numPallet = 0
-	for good in goods.keys():
-		truckWeight += distribution[good][truck] * goods[good]['weight']
-		numPallet += distribution[good][truck]
-		solver.add(0 <= distribution[good][truck])
-	solver.add(truckWeight <= 8000)
-	solver.add(numPallet <= 8)
+# Make sure that all the trucks do not have more than their max capacity of
+# pallets or more than their max capacity of weight.
+for truck in range(NUM_TRUCKS):
+    truckWeight = 0
+    numPallet = 0
 
+    for good in goods.keys():
+        truckWeight += distribution[good][truck] * goods[good]['weight']
+        numPallet += distribution[good][truck]
+        solver.add(0 <= distribution[good][truck])
+
+    solver.add(truckWeight <= TRUCK_CAPACITY)
+    solver.add(numPallet <= TRUCK_MAX_PALLETS)
+
+# Make sure that all the pallets for all the goods are distributed over the
+# trucks and optimise for the number of prittles.
 for good in goods.keys():
-	numPallet = 0
-	for truck in range(8):
-		numPallet += distribution[good][truck]
-	if (good != 'prittle'):
-		solver.add(numPallet == goods[good]['pallet'])
-	else:
-		solver.maximize(numPallet)
+    numPallet = sum(distribution[good][truck] for truck in range(NUM_TRUCKS))
+    if (good != 'prittle'):
+        solver.add(numPallet == goods[good]['pallet'])
+    else:
+        solver.maximize(numPallet)
 
-for truck in range(3,8):
-	solver.add(distribution['skipple'][truck] == 0)
+# Only three trucks (the first three) have cooling facilities for skipples.
+for truck in range(3, NUM_TRUCKS):
+    solver.add(distribution['skipple'][truck] == 0)
 
-for truck in range(8):
-	solver.add(distribution['nuzzle'][truck] <= 1)
-	solver.add(Or(distribution['prittle'][truck] == 0,
-		          distribution['crottle'][truck] == 0))
+for truck in range(NUM_TRUCKS):
+    # No two pallets of nuzzles may be in the same truck.
+    solver.add(distribution['nuzzle'][truck] <= 1)
+    # Prittles and crottles are an explosive combination.
+    solver.add(Or(distribution['prittle'][truck] == 0,
+                  distribution['crottle'][truck] == 0))
 
-with open('pallets.smt', 'w') as file:
-	print(solver.to_smt(), file=file)
+# Check if the formula is satisfiable or not.
+if solver.check():
+    model = solver.model();
 
-if (solver.check()):
-	model = solver.model();
-	print('    ', end = '')
-	for good in goods.keys():
-		print(good[0], end=' ')
-	print()
+# Print the solution in a nice table format.
+    print('    ', end = '')
+    for good in goods.keys():
+        print(good[0], end=' ')
+    print()
 
-	for truck in range(8):
-		print(truck, ": ", end='')
-		for good in goods.keys():
-			print(model[distribution[good][truck]], end=' ')
-		print()
+    for truck in range(NUM_TRUCKS):
+        print(truck, ": ", end='')
+        for good in goods.keys():
+            print(model[distribution[good][truck]], end=' ')
+        print()
 else:
-	print('unsat')
+    print('unsat')
